799c90009cdae1b1dbc9c6f3aba1edfe7775a31a
Sonam Wangchu <swangchu79@gmail.com>
2021-04-20 14:57:26 +0600

    Create program2.py
    Integration in physics education

---
+++ program2.py
@@ -0,0 +1,9 @@
+# Program Number 2:
+# A ball is dropped from a tower of height h. 
+# It has initial velocity zero and accelerates downwards under gravity. 
+# Write a program that asks the user to enter the height in meters of the tower and a time interval t in seconds, 
+# then prints on the screen the height of the ball above the ground at time t after it is dropped, ignoring air resistance. 
+# If the height of the ball is zero then print the ball hit the ground.
+# -------------------------------------------------------------------------
+#Author: Sonam Wangchu
+#Date : April 20 2021
